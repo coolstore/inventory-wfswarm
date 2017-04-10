5a2f99de3c8d2c209e3da97e2509294f60b4bd21
tqvarnst <tqvarnst@redhat.com>
2017-04-10 20:12:02 +0200

    Removed unnecessary code

--- src/main/java/com/redhat/coolstore/service/InventoryService.java
+++ src/main/java/com/redhat/coolstore/service/InventoryService.java
@@ -13,21 +13,15 @@ import com.redhat.coolstore.model.Inventory;
 @Stateless
 public class InventoryService {
 
-	@PersistenceContext
-	private EntityManager em;
+    @PersistenceContext
+    private EntityManager em;
 
-	public InventoryService() {
+    public InventoryService() {
 
-	}
-	
-	public Inventory getInventory(String itemId) {
-		Inventory inventory = em.find(Inventory.class,itemId);
-		
-//		List<String> recalledProducts = Arrays.asList("165613","165614");
-//		if (recalledProducts.contains(inventory.getItemId())) {
-//			inventory.setQuantity(0);
-//		}
-		
-		return inventory;
-	}
+    }
+
+    public Inventory getInventory(String itemId) {
+        Inventory inventory = em.find(Inventory.class, itemId);
+        return inventory;
+    }
 }
